b8194e5bad7bbff0e96ed9a285632bfb8a757be7
lelaescovar <lelaescovar@gmail.com>
2020-08-24 14:10:00 -0300

    gatitos 24

--- index.php
+++ index.php
@@ -1,1 +1,21 @@
-Hola Leela
+<!DOCTYPE html>
+<html>
+    <head>
+        <title> :) Prueba :) </title>
+        <style>
+        body{
+            background-color:red;
+            color:green;
+        }
+    
+        </style>
+    </head>
+
+    <body>
+        <h1 class="color_leela" >TEXTO DEL CUERPO, PRIMER INTENTO</h1>
+        <a href="http://www.sensotec.com.ar">Click</a>
+        <input type="submit">
+        <h1 class="color_leela" >GATITOS FELICES</h1>
+    </body>
+</html>
+ 
