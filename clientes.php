d1347675b40865d15a94a60dd6eeb90daf4e1af6
Emanuel Odstrcil <you@example.com>
2020-08-26 13:58:18 -0300

    clase4

--- clientes.php
+++ clientes.php
@@ -1,22 +1,11 @@
 <!DOCTYPE html>
 <html>
     <head>
-        <title> :) Prueba :) </title>
-        <style>
-        body{
-            background-color:#CCCCC;
-            color:#000;
-        }
-
-        .leela{
-            color:red;
-            font-size:2em;
-        }
-        </style>
+        <title>Clientes</title>
+        <link rel="stylesheet" type="text/css" href="estilos_sensotec.css">
     </head>
 
     <body>
-        
         <h1 style="color:#556677;" class="color_leela" >TEXTO DEL CUERPO, PRIMER INTENTO</h1>
         <a href="http://www.sensotec.com.ar">Click</a>
         <input type="submit">
@@ -24,12 +13,6 @@
         <p class="leela">Esto es un parrafo 1</p>
         <p class="leela" style="font-size:6em;">Esto es un parrafo 2</p>
         <p class="leela">Esto es un parrafo 3</p>
-
-        <?php
-            for($i=0; $i<=200; $i++){
-                echo "hola <br>";
-            }
-        ?>
     </body>
 </html>
  
